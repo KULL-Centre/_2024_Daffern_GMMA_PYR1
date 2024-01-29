ad15d0564418fc5d415c82f6d6cf35e290bcd61c
Kristoffer Johansson <59598936+enoee@users.noreply.github.com>
2024-01-30 00:27:17 +0100

    Add files via upload

---
+++ gmma/dia_lib1_reref/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Rscript gmma01_structure.r ../../enrichment/pyr1_dia_Y81V_D80E_lib1.csv > gmma01_structure.out
+Rscript ../gmma02_fit_individual.r > gmma02_fit_individual.out
+Rscript ../gmma03_graph.r gmma_structured.rda > gmma03_graph.out
+Rscript ../gmma04_global_estimation.r > gmma04_global_estimation.out
+Rscript ../gmma05_analysis.r gmma_fit_global.rda > gmma05_analysis.out
+
